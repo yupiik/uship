8eb8212d7072d92e9382c130cca14005fca23ab9
Romain Manni-Bucau <rmannibucau@gmail.com>
2021-08-08 18:25:01 +0200

    adding specific types for tomcat/context consumers to ensure we can produce them in CDI context and auto configure them

--- webserver-parent/webserver-cdi/src/main/java/io/yupiik/uship/webserver/cdi/TomcatWebServerProducer.java
+++ webserver-parent/webserver-cdi/src/main/java/io/yupiik/uship/webserver/cdi/TomcatWebServerProducer.java
@@ -17,6 +17,8 @@ package io.yupiik.uship.webserver.cdi;
 
 import io.yupiik.uship.webserver.tomcat.TomcatWebServer;
 import io.yupiik.uship.webserver.tomcat.TomcatWebServerConfiguration;
+import io.yupiik.uship.webserver.tomcat.customizer.ContextCustomizer;
+import io.yupiik.uship.webserver.tomcat.customizer.TomcatCustomizer;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.context.Initialized;
 import jakarta.enterprise.event.Observes;
@@ -26,6 +28,8 @@ import jakarta.enterprise.inject.Produces;
 import jakarta.servlet.ServletContainerInitializer;
 import org.apache.catalina.startup.Tomcat;
 
+import java.util.List;
+import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
@@ -40,13 +44,17 @@ public class TomcatWebServerProducer {
     @Produces
     @ApplicationScoped
     public TomcatWebServer tomcatWebServer(final Instance<TomcatWebServerConfiguration> configuration,
-                                           final Instance<ServletContainerInitializer> initializers) {
+                                           final Instance<ServletContainerInitializer> initializers,
+                                           final Instance<TomcatCustomizer> tomcatCustomizers,
+                                           final Instance<ContextCustomizer> contextCustomizers) {
         final var serverConfiguration = configuration.isResolvable() ?
                 configuration.get() : new TomcatWebServerConfiguration();
         serverConfiguration.setInitializers(Stream.concat(
                 serverConfiguration.getInitializers().stream(),
                 initializers.stream())
                 .distinct().collect(toList()));
+        serverConfiguration.setTomcatCustomizers(merge(serverConfiguration.getTomcatCustomizers(), tomcatCustomizers));
+        serverConfiguration.setContextCustomizers(merge(serverConfiguration.getContextCustomizers(), contextCustomizers));
         return new TomcatWebServer(serverConfiguration).create();
     }
 
@@ -57,5 +65,12 @@ public class TomcatWebServerProducer {
     @Produces
     public Tomcat tomcat(final TomcatWebServer server) {
         return server.getTomcat();
+    }
+
+    private <T> List<Consumer<T>> merge(final List<Consumer<T>> first, final Instance<? extends Consumer<T>> second) {
+        return Stream.concat(
+                first != null ? first.stream() : Stream.empty(),
+                second.stream())
+                .collect(toList());
     }
 }
