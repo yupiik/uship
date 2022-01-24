7f5893c85a3055a17e16e25179d787bad9157f69
Romain Manni-Bucau <rmannibucau@gmail.com>
2022-01-24 18:50:42 +0100

    adding TomcatDataSource to persistence module

---
+++ persistence/src/main/java/io/yupiik/uship/persistence/impl/datasource/tomcat/TomcatDataSource.java
@@ -0,0 +1,158 @@
+package io.yupiik.uship.persistence.impl.datasource.tomcat;
+
+import io.yupiik.uship.persistence.api.SQLFunction;
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.apache.tomcat.jdbc.pool.PoolProperties;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.function.Function;
+
+public class TomcatDataSource extends DataSource {
+    private final Class<?>[] connectionProxiedTypes = {Connection.class};
+    private final ThreadLocal<Connection> connectionThreadLocal = new ThreadLocal<>();
+
+    private TomcatDataSource(final PoolProperties properties) {
+        super(properties);
+    }
+
+    // for proxy when produced by CDI
+    protected TomcatDataSource() {
+        super(new PoolProperties());
+    }
+
+    /**
+     * Binds a connection to current thread in write mode, the result will be committed if there is no error.
+     * @param function the task to execute.
+     * @param <T> the returned type.
+     * @return the result of the function computation.
+     */
+    public <T> T write(final Function<Connection, T> function) {
+        return withConnection(connection -> {
+            final var result = function.apply(connection);
+            connection.commit();
+            return result;
+        });
+    }
+
+    /**
+     * Binds a connection to current thread in read-only mode, the result will be rolle-backed if needed.
+     * @param function the task to execute.
+     * @param <T> the returned type.
+     * @return the result of the function computation.
+     */
+    public <T> T read(final Function<Connection, T> function) {
+        return withConnection(connection -> {
+            final var readOnly = connection.isReadOnly();
+            connection.setReadOnly(true);
+            try {
+                return function.apply(connection);
+            } finally {
+                if (!readOnly) {
+                    connection.setReadOnly(false);
+                }
+                if (!connection.isClosed()) {
+                    connection.rollback();
+                }
+            }
+        });
+    }
+
+    @Override
+    public Connection getConnection() {
+        final var existing = current();
+        if (existing == null) {
+            throw new IllegalStateException("No contextual connection, ensure to use DataSourceTx around your code");
+        }
+        return existing;
+    }
+
+    public Connection current() {
+        final var connection = connectionThreadLocal.get();
+        if (connection == null) {
+            connectionThreadLocal.remove();
+        }
+        return connection;
+    }
+
+    public <T> T withConnection(final SQLFunction<Connection, T> function) {
+        try (final var connection = super.getConnection()) {
+            connectionThreadLocal.set(wrap(connection));
+            final var original = disableAutoCommit(connection);
+            try {
+                return function.apply(connection);
+            } catch (final RuntimeException | Error re) {
+                if (!connection.isClosed()) {
+                    connection.rollback();
+                }
+                throw re;
+            } finally {
+                restoreAutoCommit(connection, original);
+            }
+        } catch (final SQLException ex) {
+            throw new IllegalStateException(ex);
+        } finally {
+            connectionThreadLocal.remove();
+        }
+    }
+
+    private Connection wrap(final Connection connection) {
+        return Connection.class.cast(Proxy.newProxyInstance(
+                Thread.currentThread().getContextClassLoader(),
+                connectionProxiedTypes,
+                new ConnectionHandler(connection)));
+    }
+
+    private boolean disableAutoCommit(final Connection connection) throws SQLException {
+        final var original = connection.getAutoCommit();
+        if (original) {
+            connection.setAutoCommit(false);
+        }
+        return original;
+    }
+
+    private void restoreAutoCommit(final Connection connection, final boolean original) throws SQLException {
+        if (original) {
+            connection.setAutoCommit(true);
+        }
+    }
+
+    private static class ConnectionHandler implements InvocationHandler {
+        private final Connection connection;
+
+        private ConnectionHandler(final Connection connection) {
+            this.connection = connection;
+        }
+
+        @Override
+        public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
+            if ("close".equals(method.getName())) {
+                return null;
+            }
+            try {
+                final var result = method.invoke(connection, args);
+                if ("equals".equals(method.getName())) {
+                    return (Boolean) result ||
+                            (args[0] != null &&
+                                    Proxy.isProxyClass(args[0].getClass()) &&
+                                    areEquals(Proxy.getInvocationHandler(args[0]), this));
+                }
+                if ("hashCode".equals(method.getName())) {
+                    return connection.hashCode();
+                }
+                return result;
+            } catch (final InvocationTargetException ite) {
+                throw ite.getTargetException();
+            }
+        }
+
+        private boolean areEquals(final InvocationHandler invocationHandler, final ConnectionHandler connectionHandler) {
+            return invocationHandler instanceof ConnectionHandler &&
+                    ConnectionHandler.class.cast(connectionHandler).connection.equals(connectionHandler.connection);
+        }
+    }
+}
