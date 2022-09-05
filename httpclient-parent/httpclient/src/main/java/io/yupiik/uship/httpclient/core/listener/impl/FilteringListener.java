6e8e85d39a60c5172ed9353a58eae72d0f6d0b57
Romain Manni-Bucau <rmannibucau@gmail.com>
2022-09-05 10:40:56 +0200

    adding FilteringListener

---
+++ httpclient-parent/httpclient/src/main/java/io/yupiik/uship/httpclient/core/listener/impl/FilteringListener.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) 2021-2022 - Yupiik SAS - https://www.yupiik.com
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package io.yupiik.uship.httpclient.core.listener.impl;
+
+import io.yupiik.uship.httpclient.core.listener.RequestListener;
+
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+
+import static java.util.stream.Collectors.toList;
+
+public class FilteringListener<A> implements RequestListener<FilteringListener.State<A>> {
+    private final State<A> skippedState = new State<>(null, true);
+
+    private final Configuration<A> configuration;
+
+    public FilteringListener(final Configuration<A> configuration) {
+        this.configuration = configuration;
+    }
+
+    private boolean isSkipped(final HttpRequest request) {
+        return configuration.ignoredMethods.contains(request.method()) ||
+                configuration.ignoredPaths.stream().anyMatch(it -> it.test(request.uri().getPath()));
+    }
+
+    private boolean isSkipped(final Throwable error, final HttpResponse<?> response) {
+        if (error != null) {
+            return configuration.skipExceptions;
+        }
+        final int status = response.statusCode();
+        return status < configuration.minimumStatus || status > configuration.maximumStatus;
+    }
+
+    @Override
+    public RequestListener.State<FilteringListener.State<A>> before(final long count, final HttpRequest request) {
+        final boolean skip = isSkipped(request);
+        if (skip) {
+            return new RequestListener.State<>(request, skippedState);
+        }
+        final var state = configuration.delegate.before(count, request);
+        return new RequestListener.State<>(state.request(), new State<>(state, false));
+    }
+
+    @Override
+    public void after(final State<A> state, final HttpRequest request, final Throwable error, final HttpResponse<?> response) {
+        if (!state.skip && !isSkipped(error, response)) {
+            configuration.delegate.after(state.value.state(), state.value.request(), error, response);
+        }
+    }
+
+    public static class Configuration<T> {
+        private final RequestListener<T> delegate;
+
+        // request filters
+        private List<String> ignoredMethods = List.of();
+        private List<Predicate<String>> ignoredPaths = List.of();
+
+        // response filters
+        private boolean skipExceptions = false;
+        private int minimumStatus = 0;
+        private int maximumStatus = 1000;
+
+        public Configuration(final RequestListener<T> delegate) {
+            this.delegate = delegate;
+        }
+
+        public Configuration<T> setIgnoredPaths(final List<String> ignoredPaths) {
+            this.ignoredPaths = ignoredPaths.stream()
+                    .map(it -> (it.startsWith("regex:") ?
+                            Pattern.compile(it.substring("regex:".length())).asMatchPredicate() :
+                            (Predicate<String>) it::equals))
+                    .collect(toList());
+            return this;
+        }
+
+        public Configuration<T> setIgnoredMethods(final List<String> ignoredMethods) {
+            this.ignoredMethods = ignoredMethods;
+            return this;
+        }
+
+        public Configuration<T> setSkipExceptions(final boolean skipExceptions) {
+            this.skipExceptions = skipExceptions;
+            return this;
+        }
+
+        public Configuration<T> setMinimumStatus(final int minimumStatus) {
+            this.minimumStatus = minimumStatus;
+            return this;
+        }
+
+        public Configuration<T> setMaximumStatus(final int maximumStatus) {
+            this.maximumStatus = maximumStatus;
+            return this;
+        }
+    }
+
+    public static class State<T> {
+        private final RequestListener.State<T> value;
+        private final boolean skip;
+
+        private State(final RequestListener.State<T> value, final boolean skip) {
+            this.value = value;
+            this.skip = skip;
+        }
+    }
+}
---
+++ httpclient-parent/httpclient/src/test/java/io/yupiik/uship/httpclient/core/listener/impl/FilteringListenerTest.java
@@ -0,0 +1,232 @@
+/*
+ * Copyright (c) 2021-2022 - Yupiik SAS - https://www.yupiik.com
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package io.yupiik.uship.httpclient.core.listener.impl;
+
+import io.yupiik.uship.httpclient.core.listener.RequestListener;
+import io.yupiik.uship.httpclient.core.request.UnlockedHttpRequest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import javax.net.ssl.SSLSession;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpHeaders;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
+
+@TestInstance(PER_CLASS)
+class FilteringListenerTest {
+    private final UnlockedHttpRequest getRequest = new UnlockedHttpRequest(
+            "GET", URI.create("http://localhost:1234/test1"),
+            HttpHeaders.of(Map.of(), (a, b) -> true));
+    private final UnlockedHttpRequest postRequest = new UnlockedHttpRequest(
+            "POST", URI.create("http://localhost:1234/test2?a=b"),
+            HttpRequest.BodyPublishers.ofString("{}"),
+            HttpHeaders.of(Map.of(), (a, b) -> true));
+
+    @Test
+    void ignoreExceptions() {
+        final var collector = new Collector();
+        final var filter = new FilteringListener<>(new FilteringListener.Configuration<>(collector));
+        assertTrue(collector.collected.isEmpty());
+
+        {   // passthrough
+            final var before = filter.before(1, getRequest);
+            final var ok = response(200, getRequest);
+            filter.after(before.state(), getRequest, null, ok);
+            assertEquals(1, collector.collected.size());
+            assertEquals(ok, collector.collected.get(getRequest));
+        }
+        collector.collected.clear();
+        {   // filter
+            final var before = filter.before(1, getRequest);
+            filter.after(before.state(), getRequest, new IllegalStateException(), null);
+            assertEquals(1, collector.collected.size());
+            assertNull(collector.collected.get(getRequest));
+        }
+    }
+
+    @Test
+    void ignoreStatus() {
+        final var collector = new Collector();
+        final var filter = new FilteringListener<>(new FilteringListener.Configuration<>(collector)
+                .setMinimumStatus(200)
+                .setMaximumStatus(499));
+        assertTrue(collector.collected.isEmpty());
+
+        {   // passthrough
+            final var before = filter.before(1, getRequest);
+            final var ok = response(200, getRequest);
+            filter.after(before.state(), getRequest, null, ok);
+            assertEquals(1, collector.collected.size());
+            assertEquals(ok, collector.collected.get(getRequest));
+        }
+        collector.collected.clear();
+        {   // filter (min)
+            final var before = filter.before(1, getRequest);
+            final var ok = response(199, getRequest);
+            filter.after(before.state(), getRequest, null, ok);
+            assertEquals(1, collector.collected.size());
+            assertNull(collector.collected.get(getRequest));
+        }
+        collector.collected.clear();
+        {   // filter (max)
+            final var before = filter.before(1, getRequest);
+            final var ok = response(500, getRequest);
+            filter.after(before.state(), getRequest, null, ok);
+            assertEquals(1, collector.collected.size());
+            assertNull(collector.collected.get(getRequest));
+        }
+    }
+
+    @Test
+    void ignoreMethod() {
+        final var collector = new Collector();
+        final var filter = new FilteringListener<>(new FilteringListener.Configuration<>(collector)
+                .setIgnoredMethods(List.of("GET")));
+        assertTrue(collector.collected.isEmpty());
+
+        {   // passthrough
+            final var before = filter.before(1, postRequest);
+            final var ok = response(200, postRequest);
+            filter.after(before.state(), postRequest, null, ok);
+            assertEquals(1, collector.collected.size());
+            assertEquals(ok, collector.collected.get(postRequest));
+        }
+        collector.collected.clear();
+        {   // filter
+            final var before = filter.before(1, getRequest);
+            final var ok = response(200, getRequest);
+            filter.after(before.state(), getRequest, null, ok);
+            assertTrue(collector.collected.isEmpty());
+        }
+    }
+
+    @Test
+    void ignorePath() {
+        final var collector = new Collector();
+        final var filter = new FilteringListener<>(new FilteringListener.Configuration<>(collector)
+                .setIgnoredPaths(List.of("/test1")));
+        assertTrue(collector.collected.isEmpty());
+
+        {   // passthrough
+            final var before = filter.before(1, postRequest);
+            final var ok = response(200, postRequest);
+            filter.after(before.state(), postRequest, null, ok);
+            assertEquals(1, collector.collected.size());
+            assertEquals(ok, collector.collected.get(postRequest));
+        }
+        collector.collected.clear();
+        {   // filter
+            final var before = filter.before(1, getRequest);
+            final var ok = response(200, getRequest);
+            filter.after(before.state(), getRequest, null, ok);
+            assertTrue(collector.collected.isEmpty());
+        }
+    }
+
+    @Test
+    void ignorePathRegex() {
+        final var collector = new Collector();
+        final var filter = new FilteringListener<>(new FilteringListener.Configuration<>(collector)
+                .setIgnoredPaths(List.of("regex:/tes.1")));
+        assertTrue(collector.collected.isEmpty());
+
+        {   // passthrough
+            final var before = filter.before(1, postRequest);
+            final var ok = response(200, postRequest);
+            filter.after(before.state(), postRequest, null, ok);
+            assertEquals(1, collector.collected.size());
+            assertEquals(ok, collector.collected.get(postRequest));
+        }
+        collector.collected.clear();
+        {   // filter
+            final var before = filter.before(1, getRequest);
+            final var ok = response(200, getRequest);
+            filter.after(before.state(), getRequest, null, ok);
+            assertTrue(collector.collected.isEmpty());
+        }
+    }
+
+    private HttpResponse<String> response(final int status, final HttpRequest request) {
+        return new HttpResponse<>() {
+            @Override
+            public int statusCode() {
+                return status;
+            }
+
+            @Override
+            public HttpRequest request() {
+                return request;
+            }
+
+            @Override
+            public Optional<HttpResponse<String>> previousResponse() {
+                return Optional.empty();
+            }
+
+            @Override
+            public HttpHeaders headers() {
+                return null;
+            }
+
+            @Override
+            public String body() {
+                return "";
+            }
+
+            @Override
+            public Optional<SSLSession> sslSession() {
+                return Optional.empty();
+            }
+
+            @Override
+            public URI uri() {
+                return request.uri();
+            }
+
+            @Override
+            public HttpClient.Version version() {
+                return HttpClient.Version.HTTP_1_1;
+            }
+        };
+    }
+
+    private static class Collector implements RequestListener<Void> {
+        private final Map<HttpRequest, HttpResponse<?>> collected = new HashMap<>();
+
+        @Override
+        public State<Void> before(final long count, final HttpRequest request) {
+            collected.put(request, null);
+            return new State<>(request, null);
+        }
+
+        @Override
+        public void after(final Void state, final HttpRequest request, final Throwable error, final HttpResponse<?> response) {
+            assertTrue(collected.containsKey(request), "before should have been called before after");
+            collected.put(request, response);
+        }
+    }
+}
